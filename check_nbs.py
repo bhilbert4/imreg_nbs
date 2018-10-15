7dbca782690cbd1f783e114ee18e210be230f7b0
Erik Tollerud <erik.tollerud@gmail.com>
2018-10-15 16:42:32 -0400

    working check_nbs.py

--- check_nbs.py
+++ check_nbs.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python3
 
+import os
 import sys
 import nbformat
+import logging
 
-def fail_message(msg):
-    print('FAIL:', msg)
-
+log = logging.getLogger('check_nbs')
 
 def visit_nbs(nbpath, visitfunc):
     success = True
-    for root, dirs, files in walk(nbpath):
+    for root, dirs, files in os.walk(nbpath):
         for name in files:
-            _, ext = path.splitext(name)
-            full_path = path.join(root, name)
+            _, ext = os.path.splitext(name)
+            full_path = os.path.join(root, name)
+            if ext != '.ipynb':
+                continue
+
+            if name.startswith('exec_'): #skip the executed ones
+                continue
 
             if 'ipynb_checkpoints' in full_path:  # skip checkpoint saves
                 continue
@@ -20,19 +25,28 @@ def visit_nbs(nbpath, visitfunc):
             success = success and visitfunc(name, full_path)
     return success
 
+
 def do_checks(name, full_path):
+    log.info('Checking notebook {}'.format(name))
     success = True
     if is_executed(name, full_path):
-        fail_message('Notebook {} has executed cells!'.format(name))
+        log.error('Notebook {} has executed cells!'.format(name))
 
     return success
 
 
 def is_executed(name, full_path):
-    nbformat.read(full_path, nbformat.NO_CONVERT)
+    nb = nbformat.read(full_path, nbformat.NO_CONVERT)
+    for cell in nb.cells:
+        if cell.cell_type == 'code':
+            if cell.outputs:
+                return True
+    return False
 
 if __name__ == '__main__':
-    success = visit_nbs(do_checks)
+    logging.basicConfig()
+    log.setLevel(logging.INFO)
+    success = visit_nbs('.', do_checks)
     if not success:
         sys.exit(1)
     else:
